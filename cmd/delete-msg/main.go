9336fe42d20568710be739a6553031b5b666ec4e
Jqs7 <7@jqs7.com>
2020-02-21 00:39:11 +0800

    fix del msg func

--- cmd/delete-msg/main.go
+++ cmd/delete-msg/main.go
@@ -8,9 +8,6 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/jqs7/drei/pkg/bot"
 	"github.com/jqs7/drei/pkg/model"
 )
@@ -21,13 +18,6 @@ func main() {
 		log.Fatalf("%+v", err)
 	}
 
-	sess, err := session.NewSession()
-	if err != nil {
-		log.Fatalln("init aws session: ", err)
-	}
-	svc := sqs.New(sess)
-	queue := aws.String(os.Getenv("DELETE_MSG_QUEUE"))
-
 	lambda.Start(func(ctx context.Context, req events.SQSEvent) error {
 		for _, v := range req.Records {
 			msg := &model.MsgToDelete{}
@@ -35,13 +25,7 @@ func main() {
 				log.Println(err)
 				continue
 			}
-			if _, err := svc.DeleteMessageWithContext(ctx, &sqs.DeleteMessageInput{
-				QueueUrl:      queue,
-				ReceiptHandle: &v.ReceiptHandle,
-			}); err != nil {
-				log.Println(err)
-				return err
-			}
+			botAPI.DeleteMsg(msg.ChatID, msg.MsgID)
 		}
 		log.Printf("%+v", req)
 		return nil
